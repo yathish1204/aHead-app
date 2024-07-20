e1a93116319faf0ce296487a7ef458aface11dc5
Yathish Shettigar <142146642+yathish1204@users.noreply.github.com>
2024-07-20 23:53:25 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ParticleAnimation from "@/components/ParticleAnimation";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <ParticleAnimation/>
         <Navbar/>
+        <SpeedInsights/>
         {children}</body>
     </html>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,18 @@
 "use client"
 
 import Image from "next/image"
+import {useState} from 'react'
+import Link from "next/link"
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
+
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => {
+    setSidebar(!sidebar);
+  }
+
 const scrollToTop = () =>{
   window.scrollTo({
     top:0,
@@ -12,17 +21,34 @@ const scrollToTop = () =>{
 }
  
   return (
-    <nav className={styles.navbar} >
-      <Image onClick={scrollToTop} className={styles.navlogo} src='/assets/Image.png' alt='logo' width={50} height={50} />
-      <ul className={styles.ul}>
-        <li>Emotions</li>
-        <li>Manifesto</li>
-        <li>Self-awareness test</li>
-        <li>Work With Us</li>
-      </ul>
-      <button className={styles.btn} >Download App</button>
-      <Image src='/assets/Hamburger.png' alt='Hamburger Menu' width={72} height={72} className={styles.mobMenu} />
-    </nav>
+      <>
+        <nav >
+        <div className={styles.navbar} >
+          <Image onClick={scrollToTop} className={styles.navlogo} src='/assets/Image.png' alt='logo' width={50} height={50} />
+          <ul className={styles.ul}>
+            <li><Link href='#familiar'>Emotions</Link></li>
+            <li><Link href='#meet'>Manifesto</Link></li>
+            <li><Link href='#self'>Self-awareness test</Link></li>
+            <li><Link href='#withus' >Work With Us</Link></li>
+          </ul>
+          <button className={styles.btn} >Download App</button>
+          {sidebar ? 
+          <Image src='/assets/close.png' alt='Close Menu' width={66} height={66} className={styles.mobMenu} onClick={showSidebar} />:
+          <Image src='/assets/Hamburger.png' alt='Hamburger Menu' width={72} height={72} className={styles.mobMenu} onClick={showSidebar} />}
+
+        </div>
+   
+        {sidebar && <div className={styles.sidebar}>
+          <ul className={styles.ul}>
+              <li className={styles.li} onClick={showSidebar}><Link href='#familiar'>Emotions</Link></li>
+              <li className={styles.li} onClick={showSidebar}><Link href='#meet'>Manifesto</Link></li>
+              <li className={styles.li} onClick={showSidebar}><Link href='#self'>Self-awareness test</Link></li>
+              <li className={styles.li} onClick={showSidebar}><Link href='#withus' >Work With Us</Link></li>
+              <button onClick={showSidebar} className={styles.btn} >Download App</button>
+            </ul>
+        </div>}
+      </nav>
+      </>
   )
 }
 
