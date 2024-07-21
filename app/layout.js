176cb91fde6da86812eef7af6ddf31d18a524f34
Yathish Shettigar <142146642+yathish1204@users.noreply.github.com>
2024-07-21 21:54:46 +0530

    2nd commit

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         
         <Navbar/>
         <SpeedInsights/>
+        <Analytics/>
         {children}</body>
     </html>
   );
